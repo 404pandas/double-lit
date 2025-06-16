77e9b182cb884da92554dd30df28518d62689bcb
404pandas <mary.panda.jackson@gmail.com>
2025-06-16 18:13:55 -0400

    🎉 Challenge accepted!

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,30 +6,38 @@ import { gql } from "graphql-tag";
 import db from "../db";
 
 // ‼️ Issue located! Issue #2 + #4
-
+// Solution #2: Add displayName field to Author type and create a resolver for it.
 export const typeDefs = gql`
   type Author {
     id: ID!
     givenName: String!
     familyName: String!
+    displayName: String!
   }
 
   type Query {
     authors: [Author!]!
+    Author(givenName: String, familyName: String): String!
   }
 `;
 
 export const resolvers = {
   Query: {
     authors: () => {
-      // 🐞 Bug fix needed!
-      // We're not returning what's in DB 😱
       // 🕸️ Bug caught! Issue #1
-      // Solution- bring in the db client and use the listAuthors method!
+      // Solution #1- bring in the db client and use the listAuthors method!
       // ‼️ Issue located! Issue #8 (add Pagination)
       return db.listAuthors(); // ✅ Correct usage
     },
+    // ‼️ Issue located! Issue #2
+    // Solution #2- add displayName field to Author type. Then, add a resolver that formats the displayName based on givenName and familyName.
+     Author: {
+    displayName: (author: { givenName: string; familyName: string }) => {
+      return `${author.givenName} ${author.familyName}`;
+    },
+  },
     // ‼️ Issue located! Issue #5
+
   },
   // ‼️ Issue located! Issue #7
 };
